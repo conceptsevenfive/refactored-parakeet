515bbff04ee1c94fd3d8a260b7c5d57b69b388f1
conceptsevenfive <81585735+conceptsevenfive@users.noreply.github.com>
2021-07-13 17:13:43 +0100

    updated to v0.2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # this is a dummy main.py to test OTA
 # 
 # this is starting as version 0.1
+# we've now updated to version 0.2
 
-print("this is main.py version 0.1")
+print("this is main.py version 0.2")
