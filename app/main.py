9d070730decb9808eb4e91cf4d49e9db1de801f6
conceptsevenfive <81585735+conceptsevenfive@users.noreply.github.com>
2021-07-14 10:32:14 +0100

    update to v0.3

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 # this is a dummy main.py to test OTA
 # 
-# this is starting off as version 0.2
+# this is starting off as version 0.3
 
-print("this is app/main.py version 0.2")
+print("this is app/main.py version 0.3")
